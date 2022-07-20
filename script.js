b212e607290bc1bf20c0c7b0f227583e2c64786e
Riona Westphal <riona.westphal@gmail.com>
2022-07-20 23:44:40 +0900

    Add playRound(): plays one round of the game
    playRound() takes in the player and computer selection and determines
    whether the player wins or loses. This is displayed onto the
    console.log.

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 function getComputerChoice() {
     rand = Math.floor(Math.random() * 3);
     if(rand == 0) {
-        return "Rock";
+        return "rock";
     }
     else if(rand == 1) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch(playerSelection) {
+        case "rock": 
+            switch(computerSelection) {
+                case "rock": 
+                    console.log("Tie! Both chose rock.");
+                    break;
+                case "paper":
+                    console.log("You lose! Paper beats rock.");
+                    break;
+                case "scissors":
+                    console.log("You win! Rock beats scissors.");
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerSelection) {
+                case "rock":
+                    console.log("You win! Paper beats rock.");
+                    break;
+                case "paper":
+                    console.log("Tie! Both chose paper.");
+                    break;
+                case "scissors":
+                    console.log("You lose! Scissors beats paper.");
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerSelection) {
+                case "rock":
+                    console.log("You lose! Rock beats scissors.");
+                    break;
+                case "paper":
+                    console.log("You win! Scissors beats paper");
+                    break;
+                case "scissors":
+                    console.log("Tie! Both chose scissors");
+                    break;
+            }
+            break;
     }
 }
