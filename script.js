f7f4dd06675d4d048c864ca71468c5f8a444aba6
Riona Westphal <riona.westphal@gmail.com>
2022-07-22 23:09:39 +0900

    Display running score and announce overall winner
    playerScore and cpuScore are incremented each time the player wins or
    loses. Each player's score is displayed after each round. Once one
    player reaches 5 points, the overall winner is displayed and the game is
    over.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const playerOptions = document.querySelectorAll('.playerOptions button');
 playerOptions.forEach(button => button.addEventListener('click', getPlayerChoice));
 const result = document.querySelector('.results');
-const currentResult = document.createElement('div');
+const currentResult = document.createElement('p');
+const runningScore = document.createElement('p');
+let playerScore = 0;
+let cpuScore = 0;
+const finalWinner = document.createElement('p');
+let gameOver = false;
 
 function getComputerChoice() {
     rand = Math.floor(Math.random() * 3);
@@ -17,49 +22,70 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
+    if (gameOver) {
+        return;
+    }
     computerSelection = getComputerChoice();
     switch (playerSelection) {
         case "rock":
             switch (computerSelection) {
                 case "rock":
-                    currentResult.textContent="Tie! Both chose rock.";
+                    currentResult.textContent = "Tie! Both chose rock.";
                     break;
                 case "paper":
-                    currentResult.textContent="You lose! Paper beats rock.";
+                    currentResult.textContent = "You lose! Paper beats rock.";
+                    cpuScore++;
                     break;
                 case "scissors":
-                    currentResult.textContent="You win! Rock beats scissors.";
+                    currentResult.textContent = "You win! Rock beats scissors.";
+                    playerScore++;
                     break;
             }
             break;
         case "paper":
             switch (computerSelection) {
                 case "rock":
-                    currentResult.textContent="You win! Paper beats rock.";
+                    currentResult.textContent = "You win! Paper beats rock.";
+                    playerScore++;
                     break;
                 case "paper":
-                    currentResult.textContent="Tie! Both chose paper.";
+                    currentResult.textContent = "Tie! Both chose paper.";
                     break;
                 case "scissors":
-                    currentResult.textContent="You lose! Scissors beats paper.";
+                    currentResult.textContent = "You lose! Scissors beats paper.";
+                    cpuScore++;
                     break;
             }
             break;
         case "scissors":
             switch (computerSelection) {
                 case "rock":
-                    currentResult.textContent="You lose! Rock beats scissors.";
+                    currentResult.textContent = "You lose! Rock beats scissors.";
+                    cpuScore++;
                     break;
                 case "paper":
-                    currentResult.textContent="You win! Scissors beats paper";
+                    currentResult.textContent = "You win! Scissors beats paper";
+                    playerScore++;
                     break;
                 case "scissors":
-                    currentResult.textContent="Tie! Both chose scissors";
+                    currentResult.textContent = "Tie! Both chose scissors";
                     break;
             }
             break;
     }
     result.appendChild(currentResult);
+    runningScore.textContent = `You: ${playerScore} Opponent: ${cpuScore}`;
+    result.appendChild(runningScore);
+    if (playerScore == 5) {
+        finalWinner.textContent = "You win!!!";
+        result.appendChild(finalWinner);
+        gameOver = true;
+    }
+    if (cpuScore == 5) {
+        finalWinner.textContent = "CPU wins!!!";
+        result.appendChild(finalWinner);
+        gameOver = true;
+    }
 }
 
 function getPlayerChoice(e) {
